fc65462c3e007a576dbd1368bab7891e6a9f46f4
Jonas Caron <jonas.caron16@gmail.com>
2023-10-16 13:22:32 +0200

    sql create table

--- sql/import.sql
+++ sql/import.sql
@@ -1,1 +1,44 @@
+CREATE TABLE user (
+  user_id INT PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255),
+  registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
+CREATE TABLE profile (
+  profile_id INT PRIMARY KEY,
+  user_id INT,
+  address VARCHAR(255),
+  phone_number VARCHAR(15),
+  FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
+CREATE TABLE ticket (
+  ticket_id INT PRIMARY KEY,
+  user_id INT,
+  date_time_purchased DATETIME,
+  ticket_type VARCHAR(50),
+  price DECIMAL(10, 2),
+  FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
+CREATE TABLE merchandise (
+  merchandise_id INT PRIMARY KEY,
+  description VARCHAR(255),
+  price DECIMAL(10, 2),
+  stock INT
+);
+
+CREATE TABLE admin (
+  admin_id INT PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255)
+);
+
+CREATE TABLE ticket_type (
+  ticket_type_id INT PRIMARY KEY,
+  description VARCHAR(255),
+  price DECIMAL(10, 2)
+);
